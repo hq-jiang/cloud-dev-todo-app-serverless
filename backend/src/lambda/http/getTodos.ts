ce15d8e7528495bff7ccd8737b00e4446c602a1e
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-05-29 20:40:49 +0200

    feat: Add getTodos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,42 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const AWS = require('aws-sdk')
+
+const docClient = createDocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  const params = {
+    TableName: process.env.TODOS_TABLE,
+    IndexName: process.env.INDEX_NAME,
+    KeyConditionExpression: 'userId = :loggedInUser',
+    ExpressionAttributeValues: {
+      ':loggedInUser': '1'
+    }
+  }
+  const todos = await docClient.query(params).promise()
+  console.log(todos)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      todos
+    })
+  }
+}
+
+function createDocumentClient() {
+  if (process.env.IS_OFFLINE) {
+    console.log('Create a local DynamoDB instance')
+    return new AWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000'
+    })
+  } else {
+    return new AWS.DynamoDB.DocumentClient()
+  }
 }
