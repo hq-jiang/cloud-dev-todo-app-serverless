8ee03b722ca52005728459e98c5c7008fb27e58f
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-05-31 19:53:52 +0200

    feat: Add basic auth0 functionality

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,29 +1,46 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+import { verify } from 'jsonwebtoken'
 import { JwtPayload } from '../../auth/JwtPayload'
 
-const logger = createLogger('auth')
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDDTCCAfWgAwIBAgIJQp+sKnCVi3p9MA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
+BAMTGWRldi1rbWRjZ2YteC5ldS5hdXRoMC5jb20wHhcNMjAwNTIyMTI1MjMxWhcN
+MzQwMTI5MTI1MjMxWjAkMSIwIAYDVQQDExlkZXYta21kY2dmLXguZXUuYXV0aDAu
+Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu7d9nE/H7xWbFtoC
+Z3eADvwdOczuTzuPvO1awfvBsigfaZVsI1yHdpYwx/WC2XMtP5aYOuk58vOWd3nt
+Qi7yCPlIbZQPwtmc+NhnAAYyrWCzto0phl2PdyMcMgWBVkk8ZjPZIKqu72cLPoHp
+xLc3f/cS811PR9Oa9TEBfbYWrSDLtMnHcNhhbecPbXGzsiV9R3NkyrqHVwE0zdwQ
+WIE7ZppRzd+2fL5D5cmFtYqHpIVyb6PW16nENla2D0JxlK91xPOt0amn2GVt3WA8
+XKGTjB7uJZTlY2I5Xfq5HNvaDPBRJLSzdD1mmUnmE4S2TWSgqg1mUvLLJLfAmNvB
+FRYWFwIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBRitf6is1Jv
+Xj6qrG+KCQU17IGR9zAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
+AJjluGbH4G7Gsk+SdIfimBLx8alv5TEQ0x0lL+42I3kSET3DSyh+Zrkfyi5BIKoP
+FO+8eTclu6qImCDNkNFUS9vjtic66EMZPMMcFJNMamwfklpzfE2/IVc39S/6+Wck
+kBOQnRiVNHnNok7IcvAck0mX2gTcbnXzpRirEFs+EnSYKSXRiwa1SVCZcT8C5M2K
+pC04yq4XiTgRZRbY5p/2rtZARI8KM426XmWXS9KCuBpzA+lOS6Skv56VCJwI2MeY
+t+DajEDD/S/dxi6p7FDOBklQ52N5DwzQGtK4veHm94OM1LECZ2P/mxpJCsssdZW3
+hcERnNQfuVSHgC0SpqmTQAQ=
+-----END CERTIFICATE-----`;
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
+  const header : string = event.authorizationToken;
+  const split = header.split(' ')
+  const token = split[1]
+
   try {
-    const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
+    
+    const decodedToken = verify(
+      token,           // Token from an HTTP header to validate
+      cert,            // A certificate copied from Auth0 website
+      { algorithms: ['RS256'] } // We need to specify that we use the RS256 algorithm
+    ) as JwtPayload
+
+    console.log('User was authorized', decodedToken);
 
     return {
-      principalId: jwtToken.sub,
+      principalId: decodedToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -35,8 +52,8 @@ export const handler = async (
         ]
       }
     }
-  } catch (e) {
-    logger.error('User not authorized', { error: e.message })
+  } catch(error) {
+    console.log('User was not authorized', error.message)
 
     return {
       principalId: 'user',
@@ -52,26 +69,4 @@ export const handler = async (
       }
     }
   }
-}
-
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
-}
-
-function getToken(authHeader: string): string {
-  if (!authHeader) throw new Error('No authentication header')
-
-  if (!authHeader.toLowerCase().startsWith('bearer '))
-    throw new Error('Invalid authentication header')
-
-  const split = authHeader.split(' ')
-  const token = split[1]
-
-  return token
 }
