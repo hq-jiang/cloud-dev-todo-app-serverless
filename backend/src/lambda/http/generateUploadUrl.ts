1c1c7c08b99ec000fbd868789f2c9784a283300a
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-06-01 22:04:29 +0200

    feat: Add presigned url file upload, Add iam roles
    Add local s3, not functional yet

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,9 +2,53 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import { createLogger } from '../../utils/logger'
+
+
+const AWS = require('aws-sdk')
+
+const logger = createLogger('Lambda-generateUploadUrl')
+
+const s3 = createS3()
+
+const s3BucketName = process.env.ATTACHMENT_S3_BUCKET
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
+  const uploadUrl = s3.getSignedUrl('putObject', {
+    Bucket: s3BucketName,
+    Key: todoId,
+    Expires: '300'
+  })
+  logger.info('Signed url', {uploadUrl})
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl
+    })
+  }
+}
+
+
+function createS3() {
+  if (process.env.IS_OFFLINE) {
+    logger.info('Create a local DynamoDB instance')
+    return new AWS.S3({
+      signatureVersion: 'v4',
+      s3ForcePathStyle: true,
+      accessKeyId: 'S3RVER', // This specific key is required when working offline
+      secretAccessKey: 'S3RVER',
+      endpoint: new AWS.Endpoint('http://localhost:8001'),
+    })
+  } else {
+    return new AWS.S3({
+      signatureVersion: 'v4'
+    })
+  }
 }
